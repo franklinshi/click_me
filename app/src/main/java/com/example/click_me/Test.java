4369276471aae8db310d32151726aa63d47efbfd
Emily <1787485473@qq.com>
2019-07-23 09:39:32 +0800

    demo

---
+++ app/src/main/java/com/example/click_me/Test.java
@@ -0,0 +1,7 @@
+package com.example.click_me;
+
+import com.example.click_me.util.DBUtil;
+
+public class Test {
+
+}
---
+++ app/src/main/java/com/example/click_me/entity/news_item.java
@@ -0,0 +1,22 @@
+package com.example.click_me.entity;
+
+public class news_item {
+    String img_url;
+    String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+}
